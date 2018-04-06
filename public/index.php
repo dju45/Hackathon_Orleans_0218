56f040e70e387b78cd65f43ceb46499a123b8c97
julien <hostejulien@gmail.com>
2018-04-06 03:44:47 +0200

    modified text error

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ for($i = 0; $i < 12; $i++) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <<!-- Latest compiled and minified CSS & JS -->
+    <!-- Latest compiled and minified CSS & JS -->
     <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="../src/CSS/css-charselect.css">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
